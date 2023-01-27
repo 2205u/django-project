a49ac2f3060a4500adcfa42d5fc5507ec20c1ee1
LiquidFlare <ivinjoelabraham@gmail.com>
2023-01-27 10:17:58 +0530

    ADD: Upload static files and change base_site.html

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,8 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+from django.contrib import admin
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -9,8 +11,13 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Pubished recently?',
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return timezone.now() - datetime.timedelta(days=1) <= self.pub_date <= timezone.now() 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
